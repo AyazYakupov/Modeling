d0236aeca9d199a1c5ece2f1a58e9fc313475a36
AyazYakupov <a.yak.prog@gmail.com>
2018-11-27 12:57:07 +0300

    corrections

--- lab_3/test.py
+++ lab_3/test.py
@@ -10,10 +10,13 @@ def f(x):
 def get_x(func_val, n):
     if func_val >= 0.5:
         return 4.99999
-    i = 0.00001
+    a = 0
+    b = 5
     while True:
-        num = (fixed_quad(f, 0, i, n=n)[0] / sqrt(2 * pi))
-        if num >= func_val:
-            return round(i, 5)
-        else:
-            i += 0.00001
+        num = (fixed_quad(f, 0, (a + b) / 2, n=n)[0] / sqrt(2 * pi))
+        if num < func_val + 0.00000001 and num > func_val - 0.00000001:
+            return round((a + b) / 2, 5)
+        elif num < func_val:
+            a = (a + b) / 2
+        elif num > func_val:
+            b = (a + b) / 2
